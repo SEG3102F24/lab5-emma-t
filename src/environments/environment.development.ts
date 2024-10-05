378c7455683eaba3f8b142c5c0d1304824d0665a
Ethan <emma.tangg@gmail.com>
2024-10-04 23:31:53 -0400

    Finished lab 5

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyDU34Z1xXhHwxtzEsFIC4-wjFPc7Or2Eao",
+        authDomain: "seg3102-lab5-53060.firebaseapp.com",
+        projectId: "seg3102-lab5-53060",
+        storageBucket: "seg3102-lab5-53060.appspot.com",
+        messagingSenderId: "339455366467",
+        appId: "1:339455366467:web:2eba64bf3c73a37dfb8bf6",
+        measurementId: "G-METBMX8QGV"
+    }
+  };
+  
